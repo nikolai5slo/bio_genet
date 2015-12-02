26a5660c87ef3d355eb3ef8ea797e55ef73b74d3
Nikolai5 <nikolaj.janko@gmail.com>
2015-12-02 08:42:25 +0100

    Fix

--- simulator.py
+++ simulator.py
@@ -14,7 +14,7 @@ dt = 0.01
 # Map of activators and repressors
 gmap = np.matrix([[0,-Kd,0],
 				  [0,0,-Kd],
-		    	  [-Kd,0,0]])
+		    	  [-Kd,0,2]])
 
 # Alphas and deltas
 alphas = np.array([alpha,alpha,alpha])
@@ -22,11 +22,17 @@ deltas = np.array([delta,delta,delta])
 
 ''' Universal model generator '''
 def generate_model(gmap, alphas, deltas):
-	# Prepare matrix for computation
-	mmat = np.transpose(np.append(np.sign(gmap), np.sum(gmap, axis=0)*-1, axis=0))
+	# Prepare matrixes for computation
+	amat = np.where(gmap > 0, 1, 0) 
+	amat = np.transpose(np.append(amat, [np.sum(np.where(gmap > 0,-gmap , 0), axis=0)], axis=0))
+
+	rmat = np.where(gmap < 0, -1, 0) 
+	rmat = np.transpose(np.append(rmat, [np.sum(np.where(gmap < 0,-gmap , 0), axis=0)], axis=0))
+
 	def repr_model(p, t):
-		m = mmat.dot(np.append(p,[1]))
-		m = np.subtract(np.where(m >= 0, alphas, 0), np.multiply(deltas, p)).flatten()
+		tp = np.append(p,[1])
+		m = np.multiply(np.where(amat.dot(tp) >= 0, alphas, 0), np.where(rmat.dot(tp) >= 0, 1, 0))
+		m = np.subtract(m, np.multiply(deltas, p)).flatten()
 		return m
 
 	return repr_model
