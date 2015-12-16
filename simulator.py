9d07099a36eaf82826564923bccf2ad652b85307
Nikolai5 <nikolaj.janko@gmail.com>
2015-12-16 17:14:47 +0100

    Added perturbation

--- simulator.py
+++ simulator.py
@@ -16,6 +16,7 @@ DEGRADATION_WEIGHTS = np.array([0.6,0.3,0.1])
 
 PERTURBAIION_PROBABILITY = 0.1
 PERTURBATION_WEIGHT_DEGRADATION = np.array([0.6, 0.2, 0.2])
+PERTURBATION_WEIGHT_TYPE = np.array([0.6, 0.2, 0.2])
 
 POPULATION_SIZE = 100
 T_MAX = 20
@@ -30,7 +31,7 @@ def initiate_subject(num_proteins=5,alphas_type='scalar',deltas_type='scalar',de
 
     Kd = np.random.random_sample() * KD_MAX
     for i in range(gmap.shape[0]):
-        gmap[idx,i] = -Kd
+        gmap[idx, i] = -Kd
         idx += 1
         if idx >= num_proteins:
             idx = 0
@@ -42,16 +43,25 @@ def initiate_subject(num_proteins=5,alphas_type='scalar',deltas_type='scalar',de
         'Kd': Kd,
         'proteins': num_proteins,
         'alphas': np.random.random_sample(size=num_proteins) * ALPHA_MAX,
-        'Km': np.random.randint(1, Kd),
+        #'Km': np.random.randint(1, Kd),
         'M': gmap,
         #'M': np.zeros((num_proteins,num_proteins)),
+        'type' : np.random.randint(0, 3, size=num_proteins), # 0 - gensko izražanje, 1 - linearna modifikacija, 2 - encimska modifikacija
 
         # Degradation
-        'LD': np.random.rand(num_proteins),
-        'AD' : ad, # Matrika delt, za aktinvo degradacijo. Po diagonali so 0 ker ne more vplivati sam nase
-        'ED' : np.random.rand(2, num_proteins), # Vektorja, za encimsko degradacijo. Prvi stolpec je delta, drugi Km
-        'LM' : np.vstack((np.random.randint(-10, num_proteins, size=(num_proteins)), np.random.rand(num_proteins))),
-        'EM' : np.vstack((np.random.randint(-10, num_proteins, size=(num_proteins)), np.random.rand(num_proteins),np.random.rand(num_proteins))) #ENCIMSKA MODIFIKACIJA 3 vrstice, 1. na katerega vpliva, 2. beta, 3. Km
+        'deg_type' : np.random.randint(0, 3, size=num_proteins), # 0 - linearna deg., 1 - aktivna deg., 2 - encimska deg.
+        'deltas' : np.random.rand(num_proteins),
+        'Km' : np.random.rand(num_proteins), # Vektorja, za encimsko degradacijo. Prvi stolpec je delta, drugi Km
+
+        #'LD': np.random.rand(num_proteins),
+        #'AD' : ad, # Matrika delt, za aktinvo degradacijo. Po diagonali so 0 ker ne more vplivati sam nase
+        #'Km' : np.random.rand(num_proteins), # Vektorja, za encimsko degradacijo. Prvi stolpec je delta, drugi Km
+
+        # Modification
+        'betas' : np.random.rand(num_proteins),
+        'mod' : np.random.randint(0, num_proteins, size=(num_proteins))
+        #'LM' : np.random.randint(-10, num_proteins, size=(num_proteins))
+        #'EM' : np.vstack((np.random.randint(-10, num_proteins, size=(num_proteins)), np.random.rand(num_proteins),np.random.rand(num_proteins))) #ENCIMSKA MODIFIKACIJA 3 vrstice, 1. na katerega vpliva, 2. beta, 3. Km
     }
 
 def generate_population(size, num_proteins):
@@ -80,80 +90,100 @@ def perturbate(population):
         if np.random.rand() < PERTURBAIION_PROBABILITY:
             # TODO: dodati se sistem za nakljucno izbiro metode?
 
+
             # spreminjanje kineticnih parametrov
-            par_num = np.random.randint(len(subject['alphas']) + len(subject['LD']))
-            if par_num < len(subject['alphas']):
-                subject['alphas'][par_num] *= np.random.rand() * 2
-            else:
-                subject['alphas'][par_num - len(subject['alphas'])] *= np.random.rand() * 2
+            a_map=np.random.choice(2, len(subject['proteins']), p=[0.9, 0.1])
+            subject['alphas'] = np.where(a_map > 0, subject['alphas'] * np.random.rand() * 2, subject['alphas'])
+
+            b_map=np.random.choice(2, len(subject['proteins']), p=[0.9, 0.1])
+            subject['betas'] = np.where(b_map > 0, subject['betas'] * np.random.rand() * 2, subject['betas'])
+
+            d_map=np.random.choice(2, len(subject['proteins']), p=[0.9, 0.1])
+            subject['deltas'] = np.where(d_map > 0, subject['deltas'] * np.random.rand() * 2, subject['deltas'])
+
+            ###################################
+            # par_num = np.random.randint(len(subject['alphas']) + len(subject['LD']))
+            # if par_num < len(subject['alphas']):
+            #     subject['alphas'][par_num] *= np.random.rand() * 2
+            # else:
+            #     subject['alphas'][par_num - len(subject['alphas'])] *= np.random.rand() * 2
 
             # dodajanje novega proteina
             # TODO
 
             # spreminjanje tipa degradacije
-            degr_pos = np.random.rand()
+            subject['deg_type'][np.random.randint(0, subject['proteins'])] = np.random.choice(3, p=PERTURBATION_WEIGHT_DEGRADATION)
 
-            if degr_pos < PERTURBATION_WEIGHT_DEGRADATION[0]:
-                subject['degradation'] = 'linear'
-            elif degr_pos < np.sum(PERTURBATION_WEIGHT_DEGRADATION[0:2]):
-                subject['degradation'] = 'enzyme'
-                subject['Km'] = np.random.randint(1, subject['Kd'])
-            else:
-                subject['degradation'] = 'active'
-                # TODO: Tudi verjetno se potrebno spremeniti parametre
+            # degr_pos = np.random.rand()
+            #
+            # if degr_pos < PERTURBATION_WEIGHT_DEGRADATION[0]:
+            #     subject['degradation'] = 'linear'
+            # elif degr_pos < np.sum(PERTURBATION_WEIGHT_DEGRADATION[0:2]):
+            #     subject['degradation'] = 'enzyme'
+            #     subject['Km'] = np.random.randint(1, subject['Kd'])
+            # else:
+            #     subject['degradation'] = 'active'
+            #     # TODO: Tudi verjetno se potrebno spremeniti parametre
 
             # spreminjanje tipa generiranja protein
-            # TODO
+            subject['type'][np.random.randint(0, subject['proteins'])] = np.random.choice(3, p=PERTURBATION_WEIGHT_TYPE)
 
             # spreminjanje tipa genske regulacije
-            # TODO
+            p = np.where(subject['type'] == 0, 1.0/np.count_nonzero(subject['type']), 0)
+            idxr = np.random.choice(subject['proteins'], p=p)
+            idxc = np.random.randint(0, subject['proteins'])
+
+            oldKd = subject['M'][idxr, idxc]
+            subject['M'][idxr, idxc] = np.random.choice([0, -oldKd, oldKd * np.random.rand() * 2])
 
             # odstranjevanje proteina
-            # TODO
+            idx = np.random.randint(0, subject['proteins'])
+            subject['proteins'] -= 1
+            subject['alphas'] = np.delete(subject['alphas'], idx)
+            subject['betas'] = np.delete(subject['betas'], idx)
+            subject['deltas'] = np.delete(subject['deltas'], idx)
+            subject['type'] = np.delete(subject['type'], idx)
+            subject['deg_type'] = np.delete(subject['deg_type'], idx)
+            subject['Km'] = np.delete(subject['Km'], idx)
+            subject['mod'] = np.delete(subject['mod'], idx)
+            subject['mod'] = np.where(subject['mod'] >= idx, subject['mod']-1, subject['mod'])
+            subject['M'] = np.delete(np.delete(subject['M'], idx, axis=0), idx, axis=1)
 
     return population
 
 ''' Universal model generator '''
 def generate_model(model):
     #sestava matrike in maske za linearno modifikacijo
-    lm_vec = model['LM'].T
-    lm_map = model['LM'][0,:] >= 0
-    lm_mat = np.zeros((model['proteins'], model['proteins']))
-    for i, v in enumerate(lm_vec):
-        if(v[0] >= 0):
-            lm_mat[i, i] = -v[1]
-            lm_mat[v[0], i] = v[1]
-            lm_map[v[0]] = True
-            lm_map[i] = True
+    m_mat = np.zeros((model['proteins'], model['proteins']))
+    for i, (beta, pos, type) in enumerate(zip(model['betas'], model['mod'], model['type'])):
+        if type != 0:
+            m_mat[i, i] = -beta
+            m_mat[pos, i] = beta
 
-    #sestava matrike in maske za encimsko modifikacijo
-    em_vec = model['EM'].T
-    em_map = model['EM'][0,:] >= 0
-    em_mat = np.zeros((model['proteins'], model['proteins']))
-    for i, v in enumerate(em_vec):
-        if(v[0] >= 0):
-            em_mat[i, i] = -v[1]
-            em_mat[v[0], i] = v[1]
-            em_map[v[0]] = True
-            em_map[i] = True
+    ad_mat = -np.repeat([model['deltas']], model['proteins'], axis=0).T
+    np.fill_diagonal(ad_mat, 0)
 
     def repressilator_model(p, t):
         # Genska represija
         dg = model['alphas'] * np.prod(np.where(model['M'] != 0, (0 <= np.where(model['M'] > 0, p - model['M'], -model['M'] - p )).astype(int), 1), axis=1)
 
         # Modifikacija
-        dm = np.dot(lm_mat, p)
-        dp = np.where(lm_map, dm, dg) # linearna
+        lm = np.dot(m_mat, p) # Linearna deg
+        #dp = np.where(m_map, dm, dg) # linearna
 
         #Encimska modifikacija
-        dem_brez = p * (p / (model['EM'][2,:] + p)) # Encimska modifikacija pred mnozejem z beto
-        dem = np.dot(em_mat, dem_brez)
-        dp = np.where(em_map, dem, dg)
+        dem_brez = p * (1 / (model['Km'] + p)) # Encimska modifikacija pred mnozejem z beto
+        em = np.dot(m_mat, dem_brez)
+        #dp = np.where(em_map, dem, dp)
+
+        dp = np.where(model['type'] == 0, dg, np.where(model['type'] == 1, lm, em))
 
         # Degradacija
-        dp = np.dot(model['AD'], dp) * dp # Aktivna degradacija
-        dp = -model['LD'] * dp # Linearna degradacija
-        dp = -model['ED'][0,:] * (dp / (model['ED'][1,:] + dp)) # Encimska degradacija
+        dl = -model['deltas'] * dp # Linearna degradacija
+        da = np.dot(ad_mat, dp) * dp # Aktivna degradacija
+        de = -model['deltas'] * (dp / (model['Km'] + dp)) # Encimska degradacija
+
+        dp = np.where(model['deg_type']==0, dl, np.where(model['deg_type']==1, da, de)) # Filtered degradation
 
         return dp
     return repressilator_model
